ae5d917e16db33cd33eed45d071fbfc070ec7c35
Fahmi Rizal Kurnia <102996527+ImNash-Fahmi@users.noreply.github.com>
2024-03-19 16:11:55 +0700

    Update server-test.py

--- server-test.py
+++ server-test.py
@@ -7,21 +7,5 @@ mycredential = os.environ['mycredential']
 proxy_link = os.environ['proxy_link']
 spreadsheet_link = os.environ['spreadsheet_link']
 
-def send_messages(text):
-    url = "https://gate.whapi.cloud/messages/text"
-    
-    payload = {
-        "typing_time": 3,
-        "to": group_id,
-        "body": text
-    }
-    headers = {
-        "accept": "application/json",
-        "content-type": "application/json",
-        "authorization": authorization_whapi
-    }
-    
-    response = requests.post(url, json=payload, headers=headers)
-    print(response.text)
-
-send_messages("Workflow Test: Check")
+print(authorization_whapi)
+print(group_id)
